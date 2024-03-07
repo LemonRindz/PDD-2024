5f6eb7441d0f41f01ffd0563d4e49813bce0961c
LemonRindz <159472061+LemonRindz@users.noreply.github.com>
2024-03-06 20:42:51 -0500

    Chapter8Notes.py

--- Chapter8Notes.py
+++ Chapter8Notes.py
@@ -2,6 +2,17 @@
 #Ch. 8 Arrays
 
 #Lists
+#In Python, you create lists instead of arrays, which is similar to, but more
+#capable than the traditonal array. A list is a an object that contains multiple
+#data items. Each item stored in a list is called an element. Objects enclosed in
+#brackets ([]) and separated by commas (,) are the values of the elements.
+#Ex1. Here is a statement that creates a list of integers (int):
+#even_numbs = [2, 4, 6, 8, 10]
+#Ex2. Here is a statement that creates a list of strings:
+#names = ['Molly', 'Steven', 'Will', 'Alicia', 'Adriana']
+#You can use the 'print' function to display an entire list, as shown here:
+#numbs = [1, 2, 3, 4, 5]
+#print(numbs)
 
 #List Elements and Subscripts in Python
 
