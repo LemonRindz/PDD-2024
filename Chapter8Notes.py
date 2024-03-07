9064ec5d2eb16d86f2af6195e86f7c68ca311e71
LemonRindz <159472061+LemonRindz@users.noreply.github.com>
2024-03-06 20:26:02 -0500

    Update Chapter8Notes.py

--- Chapter8Notes.py
+++ Chapter8Notes.py
@@ -1,2 +1,16 @@
 #Imani Hollie 03/06/2024
 #Ch. 8 Arrays
+
+#Lists
+
+#List Elements and Subscripts in Python
+
+#Using 'len' function with Lists in Python
+
+#Iterating Over a List with the 'for' Loop in Python
+
+#Passing List as an Argument to a Function in Python
+
+#2-D Lists in Python
+
+#Rows and Columns
