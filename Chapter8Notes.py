9b28293da10a3f7969a71544521d5e14c757f30b
LemonRindz <159472061+LemonRindz@users.noreply.github.com>
2024-03-06 20:23:45 -0500

    Add files via upload

---
+++ Chapter8Notes.py
@@ -0,0 +1,2 @@
+#Imani Hollie 03/06/2024
+#Ch. 8 Arrays
---
+++ Practice1.py
@@ -0,0 +1,7 @@
+#Imani Hollie 03/06/2024
+#Array Practice
+
+#DEFINE ARRAY
+store = [5, 10, 15, 20, 25]
+for _ in store:
+    print(len.store)
